096ba391c9e46ac8b1dfb563e77aa2ded06e6785
root <root@host.domain>
2018-05-12 10:52:30 -0400

    init

---
+++ ProjectCI/src/test/java/com/mycompany/projectci/AppTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectci;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author kerami.ozsoy
+ */
+public class AppTest extends TestCase {
+    /**
+     * Test of initialize method, of class App.
+     */
+    public void testInitialize() {
+        System.out.println("initialize");
+        App.initialize();
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# CI
+[![Build Status](https://travis-ci.org/keramiozsoy/CI.svg?branch=master)](https://travis-ci.org/keramiozsoy/CI)
+[![codecov](https://codecov.io/gh/keramiozsoy/CI/branch/master/graph/badge.svg)](https://codecov.io/gh/keramiozsoy/CI)
